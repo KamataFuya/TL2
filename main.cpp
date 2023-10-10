10782fd8f4eaadfa6499711e8bb7735276063fbb
FuyaKamata <k021g1486@g.neec.ac.jp>
2023-10-10 15:01:19 +0900

    テクスチャファイルの読み書き①

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <cstdio>
 #include <cstdlib>
+#include <DirectXTex.h>
+#include "TextureConverter.h"
+
+//コマンドライン引数
+enum Argument {
+	kApplicationPath, // アプリケーションのパス
+	kFilePath, // 渡されたファイルのパス
+
+	NumArgument
+};
 
 int main(int argc, char* argv[]) {
 
-	// argcの数だけ繰り返す
-	for (int i = 0; i < argc; i++) {
-		//文字列argcのi番を表示
-		printf(argv[i]);
-		//改行
-		printf("\n");
-	}
+	// COM ライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+
+	//テクスチャコンバータ
+	TextureConverter converter;
+
+	//テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	// COM ライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 
